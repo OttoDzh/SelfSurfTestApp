ac7d41868480caf904e589479256215fda8f885c
Otto <jiokotok@yandex.ru>
2023-08-02 17:59:05 +0300

    Initial Commit

---
+++ SelfSurfTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SelfSurfTestApp
+//
+//  Created by Otto Dzhandzhuliya on 02.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
